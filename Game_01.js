f72ec756cf1b8de73efb9b00061027e604d0f31c
corbbing <cgappdev@gmail.com>
2013-03-01 01:20:18 -0800

    Update Game_01.js

--- Game_01.js
+++ Game_01.js
@@ -12,3 +12,27 @@ window.onload = function(){
   
 }
 
+function player(){
+  this.pos = pt(0,0);
+  this.vc = vec(0,0);
+  this.size = {w: 0, h: 0};
+}
+
+player.prototype.draw = function(ctx){
+  ctx.fillRect(this.pos.x, this.pos.y, this.size.w, this.size.h);
+  this.pos = 
+}
+
+function addVec(pt,vec){
+  var x = vec.vx + pt.x;
+  var y = vec.vy + pt.y;
+  return {x: x, y: y};
+}
+
+function vec(x,y){
+  return {vx: x, vy:y};
+}
+
+function pt(x,y){
+  return {x: x, y:y};
+}
