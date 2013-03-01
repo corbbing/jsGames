0de1ec9ad487977734777a82898c9eb904a6d168
corbbing <cgappdev@gmail.com>
2013-03-01 01:12:13 -0800

    Update Game_01.js

--- Game_01.js
+++ Game_01.js
@@ -1,3 +1,14 @@
 //Game_01.js
 
-var canvas = 
+var canvas = document.createElement("canvas")
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 512;
+document.body.apppendChild(canvas);
+
+window.onload = function(){
+  
+  ctx.fillRect(0,0,10,10);
+  
+}
+
