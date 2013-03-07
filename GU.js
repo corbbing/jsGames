4b75040c1ba5309b1d9d4933df6e4b870fd9c1d6
corbbing <cgappdev@gmail.com>
2013-03-06 20:05:33 -0800

    Update GU.js

--- GU.js
+++ GU.js
@@ -1,3 +1,6 @@
+//Game Utilities
+//insert BEFORE any other code
+
 var GU = {};
 
 GU.point = function(x,y){
@@ -7,5 +10,11 @@ GU.point = function(x,y){
 }
 
 GU.dist(pt1,pt2){
-  
+  var dx = pt2.x - pt1.x;
+  var dy = pt2.y - pt1.y;
+  return Math.sqrt(Math.pow(dx,2) + Math.pow(dy, 2));
+}
+
+GU.vec(x,y){
+  return {vx:x,vy:y};
 }
